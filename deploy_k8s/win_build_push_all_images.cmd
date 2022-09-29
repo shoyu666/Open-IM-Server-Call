8e6552d8fd79fbbd06ea88f2a1c45ae332e4bf1d
nxn <nixiaoning@163.com>
2022-09-29 14:30:49 +0800

    win build script

--- deploy_k8s/win_build_push_all_images.cmd
+++ deploy_k8s/win_build_push_all_images.cmd
@@ -1,5 +1,10 @@
 SET ROOT=%cd%
 @echo off
+echo "ensure run Open-IM-Server/scipt/win_build_all_service.cmd first"
+echo "ensure run Open-IM-Server/scipt/win_build_all_service.cmd first"
+echo "ensure run Open-IM-Server/scipt/win_build_all_service.cmd first"
+echo "ensure run Open-IM-Server/scipt/win_build_all_service.cmd first"
+echo "ensure run Open-IM-Server/scipt/win_build_all_service.cmd first"
 set version=v2.2.0
 for %%I in (
   api
@@ -20,5 +25,5 @@ for %%I in (
   push
   sdk_server
   demo
-) do  docker build -t openim/%%I:%version% --build-arg SER_NAME=%%I ../ -f temp.Dockerfile
+) do  docker build -t openim/%%I:%version% --build-arg SER_NAME=%%I.exe ../ -f temp.Dockerfile
 cd %ROOT%
